7b7d4b956055f86598c4922b4b0d1feae530e156
Vincent C <vinch.cm@gmail.com>
2023-10-21 08:21:49 +0200

    css done

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,10 +1,30 @@
 import PokemonCard from './components/PokemonCard';
 
+import "./App.css";
+
+// function App() {
+//   return (
+//     <div>
+//       <PokemonCard />
+//     </div>
+//   );
+// }
+
 function App() {
+  const container = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+
   return (
-    <div>
-      <PokemonCard />
-    </div>
+    <section style={container}>
+      <h1 style={{color: 'blue', fontWeight: 400}}>Hey! We're using inline style!</h1>
+      <div>
+        <PokemonCard />
+      </div>
+    </section>
   );
 }
 
--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -1,8 +1,18 @@
+// import "./../App.css";
+import styles from './PokemonCard.module.css';
+
 function PokemonCard() {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-    <figcaption>Bulbasaur</figcaption>
+    return <section className={styles.container}>
+        <figure>
+        <img   className="avatar"
+        style={{
+            // width: user.imageSize,
+            // height: user.imageSize,
+        }} 
+    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+    <figcaption className={styles.title}>Bulbasaur</figcaption>
     </figure>
+    </section>
 }
 
 export default PokemonCard;
